24cd5b5c36015a41a05b6b5e1ef558e3fa87d8fd
PedroR-sptech <pedro.silva048@sptech.school>
2025-06-01 16:22:55 -0300

    subindo minha url do slack

--- Java/aerodata/src/main/java/sptech/school/slack/Slack.java
+++ Java/aerodata/src/main/java/sptech/school/slack/Slack.java
@@ -11,7 +11,7 @@ public class Slack {
     private static final HttpClient client = HttpClient.newHttpClient();
 
     //  URL real do seu Webhook gerado pelo Slack
-    private static final String url = "";
+    private static final String url = "https://hooks.slack.com/services/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";
 
     public static void enviarMensagem(String mensagem) throws IOException, InterruptedException {
         JSONObject json = new JSONObject();
