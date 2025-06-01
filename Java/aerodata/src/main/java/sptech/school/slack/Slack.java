7b27450ec4e9642f78fd12300fb6d73b967ff137
NataliaAlmeidadS <nataliareg76@gmail.com>
2025-06-01 15:59:35 -0300

    Subindo slake

--- Java/aerodata/src/main/java/sptech/school/slack/Slack.java
+++ Java/aerodata/src/main/java/sptech/school/slack/Slack.java
@@ -1,4 +1,30 @@
 package sptech.school.slack;
 
+import org.json.JSONObject;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Slack {
+    private static final HttpClient client = HttpClient.newHttpClient();
+
+    //  URL real do seu Webhook gerado pelo Slack
+    private static final String url = "";
+
+    public static void enviarMensagem(String mensagem) throws IOException, InterruptedException {
+        JSONObject json = new JSONObject();
+        json.put("text", mensagem);
+
+        HttpRequest request = HttpRequest.newBuilder(URI.create(url))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.printf("Status: %s%n", response.statusCode());
+        System.out.printf("Response: %s%n", response.body());
+    }
 }
