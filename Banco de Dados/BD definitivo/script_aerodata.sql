ccfd4e96b550ee31b9fab89404634f603d20590e
DiegoLima00 <diego.flima@sptech.school>
2025-06-01 18:58:14 -0300

    selects

--- Banco de Dados/BD definitivo/script_aerodata.sql
+++ Banco de Dados/BD definitivo/script_aerodata.sql
@@ -1,4 +1,5 @@
--- DROP DATABASE aerodata;
+
+DROP DATABASE aerodata;
 CREATE DATABASE IF NOT EXISTS aerodata;
 USE aerodata;
 
@@ -19,9 +20,9 @@ CREATE TABLE Usuario (
     email VARCHAR(50) UNIQUE,
     senha CHAR(64), -- Armazena hash SHA-256
     telefone CHAR(11),
-    fk_companhia CHAR(3),  -- mudar para fk_sigla_companhia
+    fk_sigla_companhia CHAR(3),  -- mudar para fk_sigla_companhia
     CONSTRAINT fk_companhiaUsuario
-        FOREIGN KEY (fk_companhia) 
+        FOREIGN KEY (fk_sigla_companhia) 
             REFERENCES Companhia_Aerea(sigla_companhia)
 );
 
@@ -50,9 +51,13 @@ CREATE TABLE Alertas (
 		CHECK (tipo IN ('Voo cancelado', 'Voo atrasado')),
     mensagem VARCHAR(100),
     fk_voo INT,
+    fk_companhiaAlerta CHAR(3),
     CONSTRAINT fk_AlertasVoos
 		FOREIGN KEY (fk_voo) 
-			REFERENCES Voos(id_voo)
+			REFERENCES Voos(id_voo),
+	CONSTRAINT fk_AlertaCompanhia
+		FOREIGN KEY (fk_companhiaAlerta) 
+			REFERENCES Companhia_Aerea(sigla_companhia)
 );
 
 CREATE TABLE LogService (
@@ -174,3 +179,249 @@ LIMIT 10;
 -- //
 
 -- DELIMITER ;
+
+-- -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Todos os SELECT's estão referenciando algum cpf pois é a PK do usuário,
+-- (cont) logo, esta PK irá ser pega na sessionstorage e deixará as KPI's e Dash's individuais.
+
+-- total de voos
+SELECT COUNT(*) AS total_voos
+FROM Voos V
+JOIN Usuario U ON V.fk_companhia = U.fk_sigla_companhia
+WHERE U.cpf = '12345678910';
+
+ -- total de voos atrasados (%)
+SELECT 
+    ROUND(
+        (SUM(CASE 
+                WHEN V.situacao_partida LIKE 'Atraso%' 
+                  OR V.situacao_chegada LIKE 'Atraso%' 
+                THEN 1 ELSE 0 
+             END) * 100.0) / COUNT(*), 
+        2
+    ) AS porcentagem_atrasos
+FROM Voos V
+JOIN Usuario U ON V.fk_companhia = U.fk_sigla_companhia
+WHERE U.cpf = '12345678910';
+
+-- SELECT 
+--    U.cpf,
+--    COUNT(*) AS total_voos_atrasados,
+--    ROUND(
+--        COUNT(*) * 100.0 / (
+--            SELECT COUNT(*) 
+--            FROM Voos V2
+--            JOIN Usuario U2 ON V2.fk_companhia = U2.fk_sigla_companhia
+--            WHERE U2.cpf = U.cpf
+--        ), 
+--        2
+--    ) AS porcentagem_voos_atrasados
+-- FROM Voos V
+-- JOIN Usuario U ON V.fk_companhia = U.fk_sigla_companhia
+-- WHERE U.cargo = 'gerencial'
+--  AND (V.situacao_partida LIKE 'Atraso%' OR V.situacao_chegada LIKE 'Atraso%')
+-- GROUP BY U.cpf;
+
+  
+  -- total de voos cancelados
+  SELECT COUNT(*) AS total_voos_cancelados
+FROM Voos V
+JOIN Usuario U ON V.fk_companhia = U.fk_sigla_companhia
+WHERE U.cpf = '12345678910'
+  AND V.situacao_voo = 'CANCELADO';
+  
+   -- total de voos cancelados (%)
+  SELECT 
+    ROUND(
+        (SUM(CASE 
+                WHEN V.situacao_voo = 'CANCELADO' 
+                THEN 1 ELSE 0 
+             END) * 100.0) / COUNT(*), 
+        2
+    ) AS porcentagem_cancelados
+FROM Voos V
+JOIN Usuario U ON V.fk_companhia = U.fk_sigla_companhia
+WHERE U.cpf = '12345678910';
+
+-- SELECT 
+--    U.cpf,
+--    COUNT(*) AS total_voos_cancelados,
+--    ROUND(
+--        COUNT(*) * 100.0 / (
+--            SELECT COUNT(*) 
+--            FROM Voos V2
+--            JOIN Usuario U2 ON V2.fk_companhia = U2.fk_sigla_companhia
+--            WHERE U2.cpf = U.cpf
+--        ),
+--        2
+--    ) AS porcentagem_voos_cancelados
+-- FROM Voos V
+-- JOIN Usuario U ON V.fk_companhia = U.fk_sigla_companhia
+-- WHERE U.cargo = 'gerencial'
+--  AND V.situacao_voo = 'CANCELADO'
+-- GROUP BY U.cpf;
+
+  
+  -- rota com mais atrasos
+  SELECT 
+    V.sigla_aeroporto_partida,
+    V.sigla_aeroporto_destino,
+    COUNT(*) AS total_atrasos
+FROM Voos V
+JOIN Usuario U ON V.fk_companhia = U.fk_sigla_companhia
+WHERE U.cpf = '12345678910'
+  AND (
+    V.situacao_partida LIKE 'Atraso%' OR 
+    V.situacao_chegada LIKE 'Atraso%'
+  )
+GROUP BY V.sigla_aeroporto_partida, V.sigla_aeroporto_destino
+ORDER BY total_atrasos DESC
+LIMIT 1;
+
+-- rota com mais cancelamentos
+SELECT 
+    V.sigla_aeroporto_partida,
+    V.sigla_aeroporto_destino,
+    COUNT(*) AS total_cancelamentos
+FROM Voos V
+JOIN Usuario U ON V.fk_companhia = U.fk_sigla_companhia
+WHERE U.cpf = '12345678910'
+  AND V.situacao_voo = 'CANCELADO'
+GROUP BY V.sigla_aeroporto_partida, V.sigla_aeroporto_destino
+ORDER BY total_cancelamentos DESC
+LIMIT 1;
+
+-- top 3 companhias com mais atrasos nos anos de 23 e 24 (%)
+SELECT 
+    C.nome_fantasia AS companhia,
+    COUNT(CASE 
+        WHEN V.situacao_partida LIKE 'Atraso%' 
+          OR V.situacao_chegada LIKE 'Atraso%' 
+        THEN 1 
+        END) AS total_voos_atrasados,
+    
+    COUNT(*) AS total_voos,
+    
+    ROUND(
+        COUNT(CASE 
+            WHEN V.situacao_partida LIKE 'Atraso%' 
+              OR V.situacao_chegada LIKE 'Atraso%' 
+            THEN 1 
+        END) * 100.0 / COUNT(*), 
+        2
+    ) AS porcentagem_atrasos
+FROM Voos V
+JOIN Companhia_Aerea C ON V.fk_companhia = C.sigla_companhia
+WHERE YEAR(V.dia_referencia) IN (2023, 2024)
+GROUP BY C.nome_fantasia
+HAVING total_voos > 0
+ORDER BY porcentagem_atrasos DESC
+LIMIT 3;
+
+-- cancelamentos por mês
+SELECT 
+    C.nome_fantasia AS companhia,
+    YEAR(V.dia_referencia) AS ano,
+    MONTH(V.dia_referencia) AS mes,
+    COUNT(*) AS total_voos_cancelados
+FROM Voos V
+JOIN Companhia_Aerea C ON V.fk_companhia = C.sigla_companhia
+WHERE V.situacao_voo = 'CANCELADO'
+GROUP BY C.nome_fantasia, YEAR(V.dia_referencia), MONTH(V.dia_referencia)
+ORDER BY ano, mes, companhia;
+
+
+-- atrasos por mês
+SELECT 
+    C.nome_fantasia AS companhia,
+    YEAR(V.dia_referencia) AS ano,
+    MONTH(V.dia_referencia) AS mes,
+    COUNT(*) AS total_voos_atrasados
+FROM Voos V
+JOIN Companhia_Aerea C ON V.fk_companhia = C.sigla_companhia
+WHERE V.situacao_partida LIKE 'Atraso%' 
+   OR V.situacao_chegada LIKE 'Atraso%'
+GROUP BY C.nome_fantasia, YEAR(V.dia_referencia), MONTH(V.dia_referencia)
+ORDER BY ano, mes, companhia;
+
+-- total de voos por companhia
+SELECT 
+    C.nome_fantasia AS companhia,
+    COUNT(*) AS total_voos
+FROM Voos V
+JOIN Companhia_Aerea C ON V.fk_companhia = C.sigla_companhia
+GROUP BY C.nome_fantasia
+ORDER BY total_voos DESC;
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+-- Pontualidade média por companhia
+SELECT 
+    fk_companhia AS companhia,
+    COUNT(*) AS total_voos,
+    SUM(CASE 
+            WHEN situacao_partida IN ('Pontual', 'Antecipado') THEN 1 
+            ELSE 0 
+        END) AS voos_pontuais,
+    ROUND(
+        (SUM(CASE 
+                WHEN situacao_partida IN ('Pontual', 'Antecipado') THEN 1 
+                ELSE 0 
+             END) * 100.0) / COUNT(*), 2
+    ) AS percentual_pontualidade
+FROM Voos
+WHERE situacao_voo = 'REALIZADO'
+GROUP BY fk_companhia;
+
+-- tempo médio de atraso por ano
+SELECT 
+    fk_companhia AS companhia,
+    YEAR(dia_referencia) AS ano,
+    ROUND(AVG(CASE 
+        WHEN situacao_partida = 'Atraso 30-60' THEN 45
+        WHEN situacao_partida = 'Atraso 60-120' THEN 90
+        WHEN situacao_partida = 'Atraso > 120' THEN 150
+        ELSE NULL
+    END), 2) AS tempo_medio_atraso_min
+FROM Voos
+WHERE situacao_partida LIKE 'Atraso%'
+GROUP BY fk_companhia, YEAR(dia_referencia);
+
+-- total de atrasos por companhia
+SELECT 
+    c.nome_fantasia AS companhia,
+    v.sigla_aeroporto_partida,
+    v.sigla_aeroporto_destino,
+    COUNT(*) AS total_atrasos
+FROM Voos v
+JOIN Companhia_Aerea c ON v.fk_companhia = c.sigla_companhia
+WHERE v.situacao_partida LIKE 'Atraso%'
+GROUP BY 
+    c.nome_fantasia, 
+    v.sigla_aeroporto_partida, 
+    v.sigla_aeroporto_destino
+ORDER BY 
+    c.nome_fantasia, 
+    v.sigla_aeroporto_partida, 
+    v.sigla_aeroporto_destino;
+
+
+
+-- total de cancelamentos por companhia
+SELECT 
+    c.nome_fantasia AS companhia,
+    v.sigla_aeroporto_partida,
+    v.sigla_aeroporto_destino,
+    COUNT(*) AS total_cancelamentos
+FROM Voos v
+JOIN Companhia_Aerea c ON v.fk_companhia = c.sigla_companhia
+WHERE v.situacao_voo = 'CANCELADO'
+GROUP BY 
+    c.nome_fantasia, 
+    v.sigla_aeroporto_partida, 
+    v.sigla_aeroporto_destino
+ORDER BY 
+    c.nome_fantasia, 
+    v.sigla_aeroporto_partida, 
+    v.sigla_aeroporto_destino;
+
